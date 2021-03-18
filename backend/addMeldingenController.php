47d6a416e39f4bef7e75259acc48a20ba2f709cb
Faab007NL <mail@fabiandingemans.nl>
2021-03-18 10:41:43 +0100

    Betere vlidatie toegevoegd

--- backend/addMeldingenController.php
+++ backend/addMeldingenController.php
@@ -1,29 +1,49 @@
 <?php
 
 //Variabelen vullen
-$title = $_POST['title'];
-$desc = $_POST['desc'];
-$atraction = $_POST['atraction'];
-$capacity = $_POST['capacity'];
-$reporter = $_POST['reporter'];
-$datetime = date('Y-m-d H:i:s', time());
+$type = $_POST['type'];
+$overige_info = $_POST['overige_info'];
+$attractie = $_POST['attractie'];
+$capaciteit = $_POST['capaciteit'];
+$melder = $_POST['melder'];
+$gemeld_op = date('Y-m-d H:i:s', time());
+
+if(isset($_POST['prioriteit'])){
+    $prioriteit = 1;
+}else{
+    $prioriteit = 0;
+}
+
+if(!is_numeric($capaciteit)){
+    $capaciteit = 0;
+}
+
+if($capaciteit < 0){
+    $capaciteit = 0;
+}
+
+if($capaciteit > 1000){
+    $capaciteit = 1000;
+}
+
 //1. Verbinding
 require_once 'conn.php';
 
 //2. Query
-$query="INSERT INTO `meldingen`(`Title`, `Desc`, `Atraction`, `Reporter`, `Capacity`, `InsertDateTime`) VALUES(:Title, :Desc, :Atraction, :Reporter, :Capacity, :InsertDateTime)";
+$query="INSERT INTO `meldingen`(`type`, `overige_info`, `attractie`, `melder`, `capaciteit`, `gemeld_op`, `prioriteit`) VALUES(:type, :overige_info, :attractie, :melder, :capaciteit, :gemeld_op, :prioriteit)";
 
 //3. Prepare
 $statement=$conn->prepare($query);
 
 //4. Execute
 $statement->execute([
-    "Title" => $title,
-    "Desc" => $desc,
-    "Atraction" => $atraction,
-    "Reporter" => $reporter,
-    "Capacity" => $capacity,
-    "InsertDateTime" => $datetime
+    "type" => $type,
+    "overige_info" => $overige_info,
+    "attractie" => $attractie,
+    "melder" => $melder,
+    "capaciteit" => $capaciteit,
+    "gemeld_op" => $gemeld_op,
+    "prioriteit" => $prioriteit
 ]);
 
 header("Location: ../meldingen/index.php");
